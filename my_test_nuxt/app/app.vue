5ee78129db133d2c88ddfc04419a66302007e94f
yayaya-pro <1611641499@qq.com>
2025-09-14 21:37:11 +0800

    feat: 新增nuxt项目框架+element-plus

---
+++ my_test_nuxt/app/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="demo-progress">
+    <el-icon><Document /></el-icon>
+    <h1>HELLO WORLD!</h1>
+    <el-progress :text-inside="true" :stroke-width="26" :percentage="80" />
+  </div>
+</template>
+<script setup>
+import { Document } from "@element-plus/icons-vue"
+</script>
+<style lang="scss" scoped>
+.demo-progress {
+  .el-progress--line {
+    margin-bottom: 15px;
+    max-width: 600px;
+  }
+}
+</style>
---
+++ my_test_nuxt/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  modules: [
+    '@element-plus/nuxt'
+  ],
+  css: [
+    'element-plus/dist/index.css'
+  ],
+  devtools: { enabled: true }
+})
